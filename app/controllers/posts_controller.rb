8e913c1c812290a0739175f9f6a7306c5e09edf5
Ranika Williams <rnkprice07@gmail.com>
2019-10-29 20:27:11 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,7 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post = Post.new_attributes(post_params)
+    @post = Post.new(post_params)
     if @post.valid?
       @post.save
       redirect_to post_path(@post)
