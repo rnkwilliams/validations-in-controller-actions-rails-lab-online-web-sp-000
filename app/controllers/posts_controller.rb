67293616bfb2ff9e0fd918538dfc025159bc217c
Ranika Williams <rnkprice07@gmail.com>
2019-10-29 20:56:00 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,7 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post = Post.update(post_params)
+    @post.update(post_params)
     if @post.valid?
       @post.save
       redirect_to post_path(@post)
